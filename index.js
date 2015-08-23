102ca478d06cfe21fae506a167e73fe9592c397d
xpan2 <xpan@walmartlabs.com>
2015-08-23 14:53:54 -0700

    Cleanups

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 let http = require('http')
+let fs = require('fs')
+let path = require('path')
 let request = require('request')
+let through = require('through')
 let argv = require('yargs')
     .default('host', '127.0.0.1')
     .argv
 let scheme = 'http://'
-let port = argv.port || (argv.host === '127.0.0.1' ? 8000 : 80)
-let path = require('path')
-let fs = require('fs')
+let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
+let destinationUrl = argv.url || scheme + argv.host + ":" + port
 let logPath = argv.log && path.join(__dirname, argv.log)
 let logStream = logPath ? fs.createWriteStream(logPath) : process.stdout
 
 http.createServer((req, res) => {
+    logStream.write('\nEcho request: \n' + JSON.stringify(req.headers))
     for (let header in req.headers) {
     	res.setHeader(header, req.headers[header])
     }
+    through(req, logStream, {autoDestroy: false})
     req.pipe(res)
 }).listen(8000)
 
-console.log('Listening at http://127.0.0.1:8000')
-
+logStream.write('Listening at http://127.0.0.1:8000')
 
 http.createServer((req, res) => {
-    let destinationUrl = req.headers['x-destination-url'] || argv.url || scheme + argv.host + ":" + port
+    let url = req.headers['x-destination-url'] || destinationUrl
+    logStream.write(`\nProxying request to: ${url + req.url}`)
 
     let options = {
         headers: req.headers,
-        url: destinationUrl
+        url: url + req.url
     }
     options.method = req.method
 
-    logStream.write(`Proxying request to: ${destinationUrl} \n`)
+    logStream.write('\nProxy request: \n' + JSON.stringify(req.headers))
+    through(req, logStream, {autoDestroy: false})
 
-    let downstreamResponse = req.pipe(request(options))
-    logStream.write(JSON.stringify(downstreamResponse.headers))
-    logStream.write('\n');
-    downstreamResponse.pipe(logStream, {end: false})
-    downstreamResponse.pipe(res)
+    let destinationResponse = req.pipe(request(options))
+
+    logStream.write(JSON.stringify(destinationResponse.headers))
+    destinationResponse.pipe(res)
+    through(destinationResponse, logStream, {autoDestroy: false})
 
 }).listen(8001)
--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
-curl -v --url "http://127.0.0.1:8001" -d "hello proxy" -H "x-asdf: yodawg"
+#echo server: 8000, proxy server: 8001
+#run echo
+#curl -v http://127.0.0.1:8000 -d "hello world" -H "x-asdf: foo"
+
+#run proxy: 8001
+curl -v http://127.0.0.1:8001 -d "hello world" -H "x-asdf: foo"
+
+#run proxy with google as destination url and without POST
+#curl -v http://127.0.0.1:8001 -H "x-asdf: foo"
+
+#run proxy with google.com as x-destination-url 
+#curl -v http://127.0.0.1:8001 -H "x-destination-url: http://www.google.com"
